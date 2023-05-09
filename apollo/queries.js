d220e84be64f9416eb5bc23fd1ecec788250aa5f
Diego Gallardo <101141475+dgallardox@users.noreply.github.com>
2023-05-09 15:22:51 -0500

    Update queries.js
    Increase query limit to 1000

--- apollo/queries.js
+++ apollo/queries.js
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 
 export const GET_100_POSTS = gql`
   query GET_POSTS {
-    posts(first: 100) {
+    posts(first: 1000) {
       nodes {
         id
         slug
