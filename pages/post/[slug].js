4e57423c487af925ff01c3d76aceea186fb5b141
Diego J. Gallardo <101141475+dgallardox@users.noreply.github.com>
2023-09-12 00:21:57 +0000

    added 404 if not found

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,41 +1,45 @@
 import { gql } from "@apollo/client";
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
 import client from "../../apollo/apollo-client";
 import Navbar from "../../components/Navbar/Navbar";
 
 export default function Slug({ post }) {
-
   return (
     <>
       <Navbar />
-      <h2>{ post.title }</h2>
-      <p>{ post.content }</p>
+      <h2>{post.title}</h2>
+      <p>{post.content}</p>
     </>
-  )
-
+  );
 }
 
 export const getStaticPaths = async () => {
   return {
     paths: [],
-    fallback: "blocking"
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const slug = params.slug;
+
+  try {
+    const { data } = await client.query({
+      query: GET_POST,
+
+      variables: {
+        id: slug,
+      },
+    });
+  } catch (err) {
+    return {
+      notFound: true,
+    };
   }
-}
-
-export const getStaticProps = async ({params}) => {
-  const slug = params.slug
-
-  const { data } = await client.query({
-    query: GET_POST,
-
-    variables: {
-      id: slug,
-    },
-  });
 
   return {
     props: {
-      post: data.post
+      post: data.post,
     },
   };
 };
