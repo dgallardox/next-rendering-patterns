261aad08d70fe5863fa5df8ac71cea3816f0fe97
Diego J. Gallardo <101141475+dgallardox@users.noreply.github.com>
2023-11-14 09:38:58 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@
 const WORDPRESS_SITE = process.env.WORDPRESS_SITE
 
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Referrer-Policy',
+            value: 'no-referrer',
+          }
+        ],
+      },
+    ]
+  },
   reactStrictMode: true,
   images: {
     domains: [WORDPRESS_SITE],
